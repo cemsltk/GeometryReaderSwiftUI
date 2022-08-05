b7d1cb4e4175119f6657c821bceb299db13794c7
Cem Saltık <cemsltk35@gmail.com>
2022-08-06 01:13:14 +0300

    Geometry reader tests

---
+++ ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  GeoReader
+//
+//  Created by Cem Saltık on 4.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            GeometryReader { geo in
+                
+//                geo.safeAreaInsets.leading
+//                geo.safeAreaInsets.trailing
+                VStack {
+                    HStack {
+                        Text("First")
+                            .frame(width: geo.size.width/3, height: geo.size.height/4)
+                            .background(Color.blue)
+                        Text("Second")
+                            .frame(width: geo.size.width/3, height: geo.size.height/4)
+                            .background(Color.green)
+                        Text("Third")
+                            .frame(width: geo.size.width/3, height: geo.size.height/4)
+                            .background(Color.gray)
+                    }
+
+                    HStack {
+                        Text("Fourth")
+                            .frame(width: geo.size.width/2, height: geo.size.height/4)
+                            .background(Color.brown)
+                        Text("Fifth")
+                            .frame(width: geo.size.width/2, height: geo.size.height/4)
+                            .background(Color.purple)
+                    }
+                    
+                    HStack {
+//                        Text("Sixth")
+//                            .frame(width: geo.size.width/4, height: geo.size.height/4)
+//                            .background(Color.indigo)
+                        Text("Seven")
+                            .frame(width: geo.size.width/3, height: geo.size.height/4)
+                            .background(Color.yellow)
+                        Text("Eight")
+                            .frame(width: geo.size.width/3, height: geo.size.height/4)
+                            .background(Color.mint)
+                        Text("Nine")
+                            .frame(width: geo.size.width/3, height: geo.size.height/4)
+                            .background(Color.orange)
+                    }
+                    
+                    HStack {
+                        Text("Ten")
+                            .frame(width: geo.size.width, height: geo.size.height/4)
+                            .background(Color.red)
+                    }
+                }
+                
+            }
+        }
+            .background(Color.red)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+            ContentView()
+        }
+    }
+}
---
+++ GeoReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoReaderApp.swift
+//  GeoReader
+//
+//  Created by Cem Saltık on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GeoReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
